57f38bc090170364c16fe849f0245d5d94f1b882
sammytone23 <sammytone23@gmail.com>
2022-05-22 18:59:22 +1000

    added app__testing__.py for purposes of having a way to test my ideas before implementing them in main. Deleted the testing functions from app.py

--- app.py
+++ app.py
@@ -27,20 +27,6 @@ def apology(message, code=400):
 		return s
 	return render_template("apology.html", apology_message=f'{code}, '+escape(message), heading=code, session=session), code
 
-"""
-@app.route('/reset_web_db')
-def reset_people():
-	try open('web_db.db'):
-		web_db=SQL('web_db.db')
-		web_db.execute('DROP TABLE people;')
-		web_db.execute('DROP TABLE questions;')
-	except:
-		web_db=SQL('web_db.db')
-	web_db.execute('CREATE TABLE people(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR NOT NULL, isTeacher BOOLEAN, classCode VARCHAR NOT NULL, UNIQUE(classCode));')
-	web_db.execute('CREATE TABLE classes(id INTEGER PRIMARY KEY AUTOINCREMENT, class )')
-	web_db.execute('CREATE TABLE questions(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR NOT NULL, isMult BOOLEAN, multAns VARCHAR NOT NULL, hasChild BOOLEAN, hasParent BOOLEAN, hasImage BOOLEAN);')
-"""
-
 @app.after_request
 def after_request(response):
 	"""Ensure responses aren't cached"""
@@ -67,64 +53,6 @@ def home():
 @app.route('/apology')
 def apologise():
 	return apology('get apologised')
-
-
-@app.route("/register", methods=['GET','POST'])
-def login():
-
-	# Forget any user_id
-	session.clear()
-
-	if request.method=='POST':
-		 # Ensure username was submitted
-		if not request.form.get("username"):
-			return apology("must provide username", 403)
-
-		# Ensure password was submitted
-		elif not request.form.get("password"):
-			return apology("must provide password", 403)
-
-		# Ensure confirmation was submitted
-		elif not request.form.get("confirmation"):
-			return apology("must provide confirmation", 403)
-
-		# Ensure password = confirmation
-		elif not request.form.get("confirmation")==request.form.get("password"):
-			return apology("password and confirmation must match", 403)
-
-		""" < add person to db here >"""
-
-	else:
-		pass
-
-@app.route('/is_correct', methods=['POST'])
-def is_correct():
-	if not request.form.get('thing'):
-		return apology("must input thing", code=403)
-	if request.form.get('thing')=='correct':
-		session.pop('reply', None)
-		session['pages']['win']='/win'
-		session['win']=True
-	else:
-		session['reply']='that input is not correct'
-	return redirect('/')
-
-@app.route('/win')
-def win():
-	if 'win' not in session.keys() or not session['win']:
-		return redirect('/')
-	return render_template('win.html', heading='You Won!', session=session)
-
-@app.route('/add_page', methods=['GET', 'POST'])
-def add_page():
-	print(request)
-	if request.method=='POST':
-		if not request.form.get('page'):
-			return apology("must input page", code=403)
-		session['pages'][request.form.get('page')]='/'
-	
-	return render_template('add.html', heading="add a fake page", session=session)
-
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
