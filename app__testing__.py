a8026b266f42fface9cc232cdbebae2ea89e973f
sam hogan <sammytone23@gmail.com>
2022-05-25 01:07:47 +1000

    did so so much, made a functional test mark page, made it so that i store every test done, made a page for teachers to see their class's statistics for testing, and look at one student in specific for more info, fixed a bunch of bugs! Still todo: add one paper, make a home page that gives some info, maybe add some dummy data that looks believable if i have time

--- app__testing__.py
+++ app__testing__.py
@@ -1,4 +1,4 @@
-#main python app
+ #main python app
 
 from flask import Flask, render_template, redirect, session, request, url_for
 from flask_session import Session
@@ -10,7 +10,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-# web_db=SQL('web_db.db')
 
 
 def apology(message, code=400):
@@ -28,9 +27,10 @@ def apology(message, code=400):
 	return render_template("apology.html", apology_message=f'{code}, '+escape(message), heading='Error: '+str(code), session=session), code
 
 def update_pages_list(user):
-	pages={'home':'/home','logout':'/logout', 'random question':'/random'}
+	pages={'home':'/home','logout':'/logout', 'random question':'/random', 'make a test':'/make_test'}
 	if user['privileges'] in ['teacher','admin']:
 		pages['question list']='/question'
+		pages['check test history']='/check_test_history'
 	return pages
 
 def create_user(fake_db):
@@ -72,6 +72,9 @@ def after_request(response):
 	response.headers["Pragma"] = "no-cache"
 	return response
 
+@app.route('/session')
+def display_session():
+	return str(session)
 	
 @app.route('/reset')
 def reset_session():
@@ -181,10 +184,11 @@ def question(question_id):
 	with open('static/fake_db.json') as f:
 		fake_db=json.load(f)
 
-	try:
+	if len(fake_db['questions'])-1>int(question_id):
 		question=fake_db['questions'][int(question_id)]
-	except:
+	else:
 		return apology('invalid question id', 403)
+	
 	if question['multiple_choice']:
 		question['answers']={chr(ord('a')+p):answer for p,answer in enumerate(question['answers'])}
 	return render_template('question.html',heading='Question',session=session,question=question)
@@ -193,71 +197,142 @@ def question(question_id):
 def submit_question(question_id):
 	with open('static/fake_db.json') as f:
 		fake_db=json.load(f)
-	try:
+
+	if len(fake_db['questions'])-1>int(question_id):
 		question=fake_db['questions'][int(question_id)]
-	except:
+	else:
 		return apology('invalid question id', 403)
+	
 	if question['multiple_choice']:
 		correct='incorrect'
 		if question['correct_answer']==request.form.get('answer'):
 			correct='correct'
+		
+		session['submitted'].append({'question_id':int(question_id), 'correct_answer':question['correct_answer'], 'submitted_answer':request.form.get('answer'), 'marks':1 if correct=='correct' else 0})
+
 		return render_template('multiple_choice_mark.html', heading='Marks', session=session, correct=correct, correct_answer=question['correct_answer'], question_id=question_id)
+	
 	else:
+		session['submitted'].append({'question_id':int(question_id), 'marking_guide':question['marking_guide'], 'submitted_answer':request.form.get('answer')})
 		return render_template('short_answer_mark.html', heading='Marks', session=session, warning=False, answer=request.form.get('answer'), marking_guide=question['marking_guide'], marks=question['marks'], question_id=question_id)
 
 @app.route('/next_question', methods=['GET', 'POST'])
 def next_question():
-	if request.method=='POST':
-		pass
+	with open('static/fake_db.json') as f:
+		fake_db=json.load(f)
+	if 'in_test' in session.keys() and session['in_test']:
+		if request.method=='POST':
+			test_marks=int(request.form.get('marks'))
+			session['test_marks']+=test_marks
+			if not fake_db['questions'][int(request.form.get('from'))]['multiple_choice']:
+				session['submitted'][-1]['marks']=test_marks
+		if session['test_questions']:
+			next_q=session['test_questions'].pop(0)
+			print(next_q)
+			return redirect('/question/'+str(next_q['question_id']))
+		else:
+			return redirect('/test_marks')
 	return redirect('/home')
 
+@app.route('/test_marks')
+def test_marks():
+	with open('static/fake_db.json') as f:
+		fake_db=json.load(f)
+	if 'in_test' in session.keys():
+		marks=sum([int(question['marks']) for question in session['submitted']])
+		test={'user':session['user'], 'marks':marks,'test':session['submitted']}
+		fake_db['completed_tests'].append(test)
+		with open('static/fake_db.json','w') as f:
+			f.write(json.dumps(fake_db))
+		session.pop('in_test', '')
+		session.pop('test_questions', '')
+		session.pop('test_marks', '')
+		session.pop('submitted', '')
+		return render_template('test_marks.html', heading="Test results", session=session, test=test, questions=fake_db['questions'])
+	return redirect('/home')
+
+def make_test(length, tags):
+	with open('static/fake_db.json') as f:
+		fake_db=json.load(f)
+	possible_questions=[]
+
+	if tags:
+		for question in fake_db['questions']:
+			if list(set(tags) & set(question['tags'])):
+				possible_questions.append(question)
+	else:
+		for question in fake_db['questions']:
+			possible_questions.append(question)
+	
+	if len(possible_questions)<int(length):
+		return apology('Error: not enough questions with the chosen tag'+'s' if len(tags)>1 else '')
+	else:
+		choices=random.choices(possible_questions,k=int(length))
+		session['in_test']=True
+		session['test_questions']=choices
+		session['test_marks']=0
+		session['submitted']=[]
+		return redirect('/next_question')
+
+
+@app.route('/make_test', methods=['GET','POST'])
 def set_test(questions=[]):
 	with open('static/fake_db.json') as f:
 		fake_db=json.load(f)
-	pass
-
-"""
-!!! Testing pages, leave commented out !!!
-
-def make_page(route, title="Title", methods=['GET'], file=None, **kwargs):
-	if request.method not in methods:
-		return apology("method not allowed", 403)
-	if not file:
-		return render_template()
-
-@app.route('/apology')
-def apologise():
-	return apology('get apologised')
-
-
-@app.route('/is_correct', methods=['POST'])
-def is_correct():
-	if not request.form.get('thing'):
-		return apology("must input thing", code=403)
-	if request.form.get('thing')=='correct':
-		session.pop('reply', None)
-		session['pages']['win']='/win'
-		session['win']=True
-	else:
-		session['reply']='that input is not correct'
-	return redirect('/')
-
-@app.route('/win')
-def win():
-	if 'win' not in session.keys() or not session['win']:
-		return redirect('/')
-	return render_template('win.html', heading='You Won!', session=session)
-
-@app.route('/add_page', methods=['GET', 'POST'])
-def add_page():
-	print(request)
 	if request.method=='POST':
-		if not request.form.get('page'):
-			return apology("must input page", code=403)
-		session['pages'][request.form.get('page')]='/'
+		if not request.form.get('test_length'):
+			return render_template('make_test.html', heading="Test yourself", session=session, tags=tags, num_questions=len(fake_db['questions']), warning="please input a test length")
+		tags=request.form.getlist('tags')
+		return make_test(request.form.get('test_length'),tags)
 	
-	return render_template('add.html', heading="add a fake page", session=session)
-"""
+	tags=[]
+	for question in fake_db['questions']:
+		tags=list(set(tags)|set(question['tags']))
+
+	return render_template('make_test.html', heading="Test yourself", session=session, tags=tags, num_questions=len(fake_db['questions']), warning=False)
+
+def get_user_stats(user_id):
+	with open('static/fake_db.json') as f:
+		fake_db=json.load(f)
+	tests_done=0
+	marks_got=0
+	possible_marks=0
+	for test in fake_db['completed_tests']:
+		if test['user']==user_id:
+			tests_done+=1
+			for question in test['test']:
+				marks_got+=question['marks']
+				possible_marks+=fake_db['questions'][question['question_id']]['marks']
+	if tests_done:
+		return{'user':user_id,'test_num':tests_done,'avg_marks':round((marks_got/possible_marks)*100)}
+	else:
+		return{'user':user_id,'test_num':0,'avg_marks':0}
+
+
+@app.route('/check_test_history', methods=['GET','POST'])
+def check_test_history():
+	with open('static/fake_db.json') as f:
+		fake_db=json.load(f)
+	if request.method=='POST':
+		if request.form.get('student_id'):
+			student_id=int(request.form.get('student_id'))
+			if student_id>len(fake_db['logins']) or fake_db['logins'][session['user']]['privileges']=='teacher' and fake_db['logins'][int(request.form.get('student_id'))]['class']!=fake_db['logins'][session['user']]['class']:
+				return render_template('student_search.html', heading="View test results", session=session, results=False, student=False, warning='That is not a student in your class')
+			tests_by_student=[test for test in fake_db['completed_tests'] if test['user']==student_id]
+			return render_template('student_search.html', heading="View test results", session=session, results={'user_id':student_id, 'tests':tests_by_student}, student=True, warning=False, questions=fake_db['questions'])
+		user_class=fake_db['logins'][session['user']]['class']
+		user_stats=[]
+		for user in fake_db['logins']:
+			if user['class']==user_class or user_class==0:
+				user_stats.append(get_user_stats(user['user_id']))
+
+		return render_template('student_search.html', heading="View test results", session=session, results={'class':user_class, 'users':user_stats}, student=False, warning=False)
+
+
+
+	return render_template('student_search.html', heading="View test results", session=session, results=False, student=False, warning=False)
+
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
